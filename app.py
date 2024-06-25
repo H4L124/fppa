94535d5e6bff079a864537de6b0d368bbd9ba30e
Nur Haliza Rositasari <97608900+H4L124@users.noreply.github.com>
2024-06-25 17:05:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from sklearn.preprocessing import StandardScaler
 st.set_page_config(page_title="Dashboard Klasifikasi SVM dan KMeans SVM")
 
 # Cache the data loading function to avoid reloading the data on each rerun
-@st.cache
+@st.cache_resource
 def load_data(file_path):
     data = pd.read_excel(file_path, sheet_name='data')
     train_data = pd.read_excel(file_path, sheet_name='oversample.train')
@@ -23,13 +23,13 @@ def load_data(file_path):
 data, train_data, test_data = load_data('data.xlsx')
 
 # Cache the model training functions to avoid retraining the models on each rerun
-@st.cache(allow_output_mutation=True)
+@st.cache_resource(allow_output_mutation=True)
 def train_svm(X_train_svm, y_train_svm, kernel='linear', C=1.0, gamma='scale'):
     svm_model = SVC(kernel=kernel, C=C, gamma=gamma)
     svm_model.fit(X_train_svm, y_train_svm)
     return svm_model
 
-@st.cache(allow_output_mutation=True)
+@st.cache_resource(allow_output_mutation=True)
 def train_kmeans_svm(X_train_ksvm, y_train_ksvm, n_clusters=3, kernel='linear', C=1.0, gamma='scale'):
     kmeans = KMeans(n_clusters=n_clusters, random_state=42)
     kmeans.fit(X_train_ksvm)
